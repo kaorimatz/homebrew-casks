5839e1bef2f891e6a6e29486eb1a0b097d0cc130
Satoshi Matsumoto <kaorimatz@gmail.com>
2014-06-28 16:57:25 +0900

    Initial commit

---
+++ Casks/aquaskk.rb
@@ -0,0 +1,10 @@
+class Aquaskk < Cask
+  version '4.2'
+  sha256 '2370ba379a1b5f4e5cc3bf3821ba396b713b7ff7ebfa0c2324c7a4c933324f1d'
+
+  url "http://dl.sourceforge.jp/aquaskk/58814/AquaSKK-#{version}.dmg"
+  homepage 'http://aquaskk.sourceforge.jp/'
+
+  install 'AquaSKK.pkg'
+  uninstall :pkgutil => 'jp.surceforge.inputmethod.aquaskk.pkg'
+end
